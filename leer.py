785889096e7f11689041b01f098a67974e1bda5e
MaureenArg <maureenarg@gmail.com>
2020-09-17 17:58:33 -0600

    agregar programa para buscar maravillas en Grecia

--- leer.py
+++ leer.py
@@ -1,5 +1,6 @@
 # Recorrido e impresión de las líneas de un archivo de texto
 with open("maravillas_antiguas.csv", "r") as archivo:
     for linea in archivo:
-        print(linea, end='')
+        if (linea.find ("Grecia") != -1):
+            print(linea, end='')
         
